e994d3e34c37f5a05eda654cec2460567d420774
cselmi <rm@192.168.1.4>
2020-11-16 17:04:53 +0100

    2020 11 16

--- m4/ground/GUI.py
+++ m4/ground/GUI.py
@@ -3,7 +3,7 @@ Autors
   - C. Selmi: written in 2019
 '''
 
-import numpy as np
+import numpy as npax 
 from guietta import Gui, M, _, ___, III, VS
 from guietta import Ax
 
@@ -21,14 +21,16 @@ class GuiCreator():
 
     def create_one_image(self, image):
         with Ax(self._gui.plot) as ax:
+            ax.clear()
             ax.set_title('Titolo')
             im = ax.imshow(image, origin='lower', cmap='Reds')
             ax.figure.colorbar(im, ax=ax)
 
+
     def plot_image(self, image):
-        self.create_one_image(image)
         self._gui.events(
             [  _            ,  _ , _ ,   self.create_one_image ],
             [  _            ,  _ , _ ,   _          ],
             [  _            ,  _ , _ ,   _          ], )
+        self.create_one_image(image)
         self._gui.run()
