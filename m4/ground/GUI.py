3d47f1831f9c7f5abbf05988c086495b1e9668ce
cselmi <rm@172.20.10.7>
2020-11-17 17:17:31 +0100

    2020 11 17

--- m4/ground/GUI.py
+++ m4/ground/GUI.py
@@ -3,7 +3,9 @@ Autors
   - C. Selmi: written in 2019
 '''
 
-import numpy as npax 
+import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import interact, interactive
 from guietta import Gui, M, _, ___, III, VS
 from guietta import Ax
 
@@ -26,7 +28,6 @@ class GuiCreator():
             im = ax.imshow(image, origin='lower', cmap='Reds')
             ax.figure.colorbar(im, ax=ax)
 
-
     def plot_image(self, image):
         self._gui.events(
             [  _            ,  _ , _ ,   self.create_one_image ],
@@ -34,3 +35,13 @@ class GuiCreator():
             [  _            ,  _ , _ ,   _          ], )
         self.create_one_image(image)
         self._gui.run()
+
+    def test(self, color='Blues'):
+        fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+        ax.set_title('Titolo')
+        im = ax.imshow(self._image, origin='lower', cmap=color)
+        ax.figure.colorbar(im, ax=ax)
+
+    def piùColor(self, image, color='Blues'):
+        self._image = image
+        interactive(self.test, {'manual': True}, color=['Blues', 'Reds', 'Greens'])
