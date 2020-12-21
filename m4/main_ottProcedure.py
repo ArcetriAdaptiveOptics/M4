434f8ab5ab99fa58c0e779c37cb8c98b952f2919
cselmi <rm@192.168.1.4>
2020-12-21 16:50:52 +0100

    20201221 dimenticato

---
+++ m4/main_ottProcedure.py
@@ -0,0 +1,68 @@
+'''
+Authors
+  - C. Selmi: written in 2020
+
+List of contents:
+
+ OTT procedures
+++++++++++++++
+- tutte le altre (da fare)
+'''
+
+#PROCEDURE OTT#
+def RS_verification():
+    #caliball
+
+# Mount the reference sphere in front on the RS
+# Align the reference sphere to the RS
+# Acquire a measurement
+# Rotate the reference sphere
+# Acquire a measurement
+# Repeat points 4-5 until the residual measurement noise is below Test Pass Criteria
+# Average the result to obtain the RS cavity
+    pass
+
+def PAR_verification():
+# Mount the PAR
+# Mount the INT
+# Mount the CGH in front of the interferometer
+# Align the CGH to the INT
+# Align the INT+CGH to the PAR
+# Acquire the measurement
+# Post process the phasemaps to average WFE and WFE noise
+    pass
+
+def LAI_verification():
+# To null the fringes:
+#
+# Mount the PAR
+# Mount the RM and place it at the PAR center
+# Remove the reference sphere
+# Place a pinhole at the L1 focus
+# Align the RM to have beam and return passing through the pinhole
+# Align the RM to null the tilt fringes on the interferometer screen
+# Align the PM and RM together to null coma on the interferometer
+#
+#
+# To center the pupil:
+#
+# Identify the markers of the PM and its center.
+# Adjust the pupil relay table to move it to the CCD center. Move table XY.
+# Adjust the table tip/tilt to null the fringes.
+    pass
+
+def SAI_verification():
+# Mount the flat reference mirror in front on the SAI. Adjust the flat tip/tilt
+# Align the flat to the SAI
+# Acquire a measurement
+# Repeat point 3 until the residual measurement noise is below the Test Pass Criteria
+# Average the result to obtain the SAI cavity WFE
+    pass
+
+def SPL_verification():
+    #test 0087
+    pass
+
+# prendere ispirazione dal sito adoptica
+def acquire_IFFunction():
+    pass
