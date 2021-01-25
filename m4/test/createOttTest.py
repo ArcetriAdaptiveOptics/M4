54406a0638eddec2b084ab74a6f42bac3308d71f
cselmi <rm@192.168.1.4>
2021-01-25 17:28:59 +0100

    2021 01 25 test e travis

---
+++ m4/test/createOttTest.py
@@ -0,0 +1,21 @@
+'''
+Authors
+  - C. Selmi: written in 2020
+'''
+import unittest
+
+class TestCalc(unittest.TestCase):
+
+    def setUp(self):
+        from m4.configuration.create_ott import OTT
+        self.ott = OTT()
+
+    def tearDown(self):
+        del self.ott
+
+    def testTower(self):
+        self.ott.slide()
+        self.ott.rslide()
+        self.ott.angle()
+        self.ott.parab()
+        self.ott.refflat()
