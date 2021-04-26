0f165f832820fa6392d0b0dc8f7c7ba14c6618ba
labot <labot@m4ws>
2021-04-26 11:13:41 +0200

    2021 04 26 modifica rebin acc data

--- m4/utils/accelerometer_data.py
+++ m4/utils/accelerometer_data.py
@@ -178,6 +178,24 @@ def plot_power_spectrum(spe, freq, tt=None):
     plt.pause(0.01)
     plt.legend()
     plt.grid()
+ 
+def rebin(a, *args):
+    '''rebin ndarray data into a smaller ndarray of the same rank whose dimensions
+    are factors of the original dimensions. eg. An array with 6 columns and 4 rows
+    can be reduced to have 6,3,2 or 1 columns and 4,2 or 1 rows.
+    example usages:
+    >>> a=rand(6,4); b=rebin(a,3,2)
+    >>> a=rand(6); b=rebin(a,2)
+    '''
+    shape = a.shape
+    lenShape = len(shape)
+    factor = np.asarray(shape)/np.asarray(args)
+    evList = ['a.reshape('] + \
+             ['args[%d],factor[%d],'%(i,i) for i in range(lenShape)] + \
+             [')'] + ['.sum(%d)'%(i+1) for i in range(lenShape)] + \
+             ['/factor[%d]'%i for i in range(lenShape)]
+    print ''.join(evList)
+    return eval(''.join(evList))
     
 
 def main(recording_seconds=5, plot_seconds=10):
@@ -185,6 +203,10 @@ def main(recording_seconds=5, plot_seconds=10):
     print(tt)
     data = read_acc_data(tt)
     vec = data[:, 5:9]
+ #   v_list=[]
+ #   for i in range(vec.shape[1]):
+ #       v_list.append(acc.rebin(vec[:,i], 1000))
+ #   vec2 = np.array(v_list)
     spe, freq = power_spectrum(vec)
     plot_power_spectrum(spe, freq)
     plt.pause(plot_seconds)
