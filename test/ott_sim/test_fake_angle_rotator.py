5ebc7f94f7a30b954f5c09e38acdd422cd7f14c2
cselmi <rm@192.168.1.5>
2021-05-06 14:33:04 +0200

    test per angle rotator (opcua e fake)

---
+++ test/ott_sim/test_fake_angle_rotator.py
@@ -0,0 +1,20 @@
+'''
+Authors
+  - C. Selmi: written in 2020
+'''
+import unittest
+from m4.ott_sim.fake_angle_rotator import FakeAngleRotator
+
+
+class TestFakeAngleRotator(unittest.TestCase):
+
+    def testMoveRelativeToCurrentPosition(self):
+        self.angle = FakeAngleRotator()
+        init_pos = 42
+        self.angle.setPosition(init_pos)
+
+        off = 7
+        curr_pos = self.angle.getPosition()
+        fin_pos = self.angle.setPosition(curr_pos + off)
+
+        self.assertAlmostEqual(init_pos + off, fin_pos)
