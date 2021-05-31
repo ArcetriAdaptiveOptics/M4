7ea2da9a8f27dd50ac9028a67d817906a18f0ee2
cselmi <rm@172.20.10.7>
2021-05-31 10:18:14 +0200

    log per le info sulla torre e funzioni di time history

--- m4/ground/ottInformation.py
+++ m4/ground/ottInformation.py
@@ -2,14 +2,73 @@
 Authors
   - C. Selmi:  written in 2021
 '''
+import time
+import os
+import numpy as np
+from m4.configuration.config import fold_name
+from m4.configuration.ott_parameters import OpcUaParameters
 
-def ciccio():
-    from m4.configuration.create_ott import OTT
-    ott = OTT()
+class OttInformation():
 
-    angle = ott.angle()
-    rslide = ott.rslide()
-    slide = ott.slide()
-    par = ott.parab()
-    rm = ott.refflat()
-    temperature = ott.temperature()
+    def __init__(self, ott, interf):
+        """The constructor """
+        self._ott = ott
+        self._interf = interf
+        self._angle = None
+        self._rslide = None
+        self._slide = None
+        self._par = None
+        self._rm = None
+        self._temperature = None
+
+    def acquireAndLogOttPositions(self):
+        self._angle = self._ott.angleRotator.getPosition()
+        self._rslide = self._ott.referenceMirrorSlider.getPosition()
+        self._slide = self._ott.parabolaSlider.getPosition()
+        self._par = self._ott.parabola.getPosition()
+        self._rm = self._ott.referenceMirror.getPosition()
+        self._temperature = self._ott.temperature.getTemperature()
+
+        self._informationLog()
+
+    def temperatureTimeHistory(self, n_measure, delay_in_seconds):
+        temp_matrix = np.zeros((n_measure, OpcUaParameters.num_PT_sensor))
+        for i in range(n_measure):
+            temp_matrix[i, :] = self._ott.temperature.getTemperature()
+            time.sleep(delay_in_seconds)
+        return temp_matrix
+
+    def interferogramTimeHistory(self, n_measure, delay_in_seconds):
+        cube = None
+        for i in range(n_measure):
+            image = self._interf.acquire_phasemap()
+            if cube is None:
+                cube = image
+            else:
+                cube = np.ma.dstack((cube, image))
+            time.sleep(delay_in_seconds)
+        return cube
+
+    def _informationLog(self):
+        file_name = os.path.join(fold_name.LOG_ROOT_FOLDER,
+                                 'OttInformationsLog.txt')
+        file = open(file_name, 'a+')
+        file.write('Angle = %f ' %self._angle)
+        file.write('\n')
+        file.write('RM slide = %f ' %self._rslide)
+        file.write('\n')
+        file.write('PAR slide = %f ' %self._slide)
+        file.write('\n')
+        file.write('RM position =')
+        for i in range(self._rm.size):
+            file.write(' %f ' %self._rm[i])
+        file.write('\n')
+        file.write('PAR position =')
+        for i in range(self._par.size):
+            file.write(' %f ' %self._par[i])
+        file.write('\n')
+        file.write('Temperature =')
+        for i in range(self._temperature.size):
+            file.write(' %f ' %self._temperature[i])
+        file.write('\n')
+        file.close()
