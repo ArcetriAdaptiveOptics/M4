a1669ab69cf9c01f15457138e7d610fc68f18a16
cselmi <rm@192.168.1.4>
2021-07-23 14:28:22 +0200

    Cambiato file per testare i conglitti nella pull request

--- m4/ground/tracking_number_folder.py
+++ m4/ground/tracking_number_folder.py
@@ -1,20 +1,22 @@
 '''
 Authors
-  - C. Selmi:  written in 2019
+  - C. Selmi: written in 2019
+             modified in July 2021
 '''
 
 import os
+import fnmatch
 from m4.ground.timestamp import Timestamp
+from m4.configuration.config import path_name
 
 
-
-def makeTrackingNumberFolder(store_in_folder):
+def createFolderToStoreMeasurements(store_in_folder):
     """
+    Create a new folder using the generation of the tracking number
     Parameters
     ----------
-    store_in_folder:string
-        path location for new tracking number folder
-
+    store_in_folder: string
+                    path where to create new fold
     Returns
     -------
         dove: string
@@ -29,3 +31,57 @@ def makeTrackingNumberFolder(store_in_folder):
     else:
         os.makedirs(dove)
     return dove, tt
+
+def findTrackingNumberPath(tt):
+    ''' Function to find the entire path from tracking number
+    Parameters
+    ----------
+        tt: string
+            tracking number
+    Returns
+    -------
+        final_path: string
+            total path
+    '''
+    rootPath = path_name.OPT_DATA_FOLDER
+    pattern = tt
+
+    for root, dirs, files in os.walk(rootPath):
+        for directory in fnmatch.filter(dirs, pattern):
+            final_path = os.path.join(root, directory)
+    try:
+        return final_path
+    except:
+        raise OSError('Tracking number %s does not exists' % tt)
+
+# '''
+# Authors
+#   - C. Selmi:  written in 2019
+# '''
+# 
+# import os
+# from m4.ground.timestamp import Timestamp
+# 
+# 
+# 
+# def makeTrackingNumberFolder(store_in_folder):
+#     """
+#     Parameters
+#     ----------
+#     store_in_folder:string
+#         path location for new tracking number folder
+# 
+#     Returns
+#     -------
+#         dove: string
+#             all path generated
+#         tt: string
+#             only tracking number
+#     """
+#     tt = Timestamp.now()
+#     dove = os.path.join(store_in_folder, tt)
+#     if os.path.exists(dove):
+#         raise OSError('Directory %s exists' % dove)
+#     else:
+#         os.makedirs(dove)
+#     return dove, tt
