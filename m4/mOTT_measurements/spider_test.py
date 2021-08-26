307814452751f4e559faccc30cd6386b3a6c0295
labot <labot@m4ws>
2021-08-26 17:32:45 +0200

    modifiche per cp e cr

--- m4/mOTT_measurements/spider_test.py
+++ m4/mOTT_measurements/spider_test.py
@@ -90,7 +90,8 @@ class SpiderTest():
 
         cp_mat = np.zeros((np.int(n_images/2), mask.shape[2]))
         cr_mat = np.zeros((np.int(n_images/2), mask.shape[2]))
-        diff_list = []
+        cp_val = cp_mat.copy()
+        #diff_list = []
         for i in range(n_images):
             if i % 2 == 0:
                 fits_file_path = os.path.join(self._fitsDirectory, dataFitsFolfer, '%04d.fits' %i)
@@ -98,18 +99,21 @@ class SpiderTest():
                 fits_file_path = os.path.join(self._fitsDirectory, dataFitsFolfer, '%04d.fits' %(i+1))
                 ima1 = read_data.readFitsSlimImage(fits_file_path)
                 diff = ima1 -ima0
-                diff_list.append(diff)
+                #diff_list.append(diff)
                 
                 for j in range(mask.shape[2]):
                     k = np.int(i/2)
                     prod = np.ma.masked_array(diff, mask=np.invert(mask[:,:,j].astype(bool)))
                     cp = prod.mean()
+                    cp_val[k,j] = cp
                     if cp > 200e-09 or cp < -200e-09:
                         cp_mat[k, j]=1
                     cr = prod.std()
                     if cr > 20e-09 or cr < -20e-09:
                         cr_mat[k, j]=1
-        return diff_list, cp_mat, cr_mat
+        dev = np.std(cp_val, 1)
+        num = (dev > 20e-09).sum()
+        return cp_mat, cr_mat, cp_val, num
 
 ###
 def main0713TestSpider(numberOfFrames, measurementsDirectory):
@@ -128,7 +132,7 @@ def main0713TestSpider(numberOfFrames, measurementsDirectory):
                                                                             rawFramesDirectory)
     return measurementsDirectory
 
-def main0908ConvertionAndAnalysis():
+def main0908ConvertionAndAnalysis(tt_list):
     sp = SpiderTest()
     # tt_list = ['20210721_103804_20210720-spider6',
     #            '20210721_104344_20210720-spider6',
@@ -142,23 +146,64 @@ def main0908ConvertionAndAnalysis():
     #            '20210721_104858_20210720-spider10', #4
     #            '20210721_111701_20210720-spider10'] #9
 
-    tt_list = ['20210721_103804_20210720-spider5',
-               '20210721_104344_20210720-spider5',
-               '20210721_104858_20210720-spider5',
-               '20210721_111701_20210720-spider5',
-               '20210721_103804_20210720-spider11',
-               '20210721_104344_20210720-spider11',
-               '20210721_104858_20210720-spider11',
-               '20210721_111701_20210720-spider11',
-               '20210721_103804_20210720-spider12',
-               '20210721_104344_20210720-spider12',
-               '20210721_104858_20210720-spider12',
-               '20210721_111701_20210720-spider12']
+    # tt_list = ['20210721_103804_20210720-spider5',
+    #            '20210721_104344_20210720-spider5',
+    #            '20210721_104858_20210720-spider5',
+    #            '20210721_111701_20210720-spider5',
+    #            '20210721_103804_20210720-spider11',
+    #            '20210721_104344_20210720-spider11',
+    #            '20210721_104858_20210720-spider11',
+    #            '20210721_111701_20210720-spider11',
+    #            '20210721_103804_20210720-spider12',
+    #            '20210721_104344_20210720-spider12',
+    #            '20210721_104858_20210720-spider12',
+    #            '20210721_111701_20210720-spider12']
+    
+    # tt_list = ['20210812_123640-20210812_Q_2x2_05',
+    #            '20210812_123640-20210812_Q_2x2_10',
+    #            '20210812_123640-20210812_G_2x2_05',
+    #            '20210812_123640-20210812_G_2x2_10',
+    #            '20210812_123640-20210812_G_2x2_15',
+    #            '20210812_123640-20210812_G_2x2_20'] 
+    # tt_list = ['20210812_123640-20210812_Q_3x3_05',
+    #            '20210812_123640-20210812_Q_3x3_10'] #mancano
+    # tt_list = ['20210812_124218-20210812_Q_2x2_05',
+    #            '20210812_124218-20210812_Q_2x2_10',
+    #            '20210812_124218-20210812_G_2x2_05',
+    #            '20210812_124218-20210812_G_2x2_10',
+    #            '20210812_124218-20210812_G_2x2_15',
+    #            '20210812_124218-20210812_G_2x2_20',
+    #            '20210812_124218-20210812_Q_3x3_05',
+    #            '20210812_124218-20210812_Q_3x3_10']
     for tt in tt_list:
         print('Converting tt = %s' %tt)
         sp.convert4DDataToSlimFits(tt)
         print('Analysing tt = %s' %tt)
-        diff, cp, cr = sp.analysis(tt)
-        print(np.sum(cp, 0).max())
+        cp, cr = sp.analysis(tt)
+        print('cp = %d' %(np.sum(cp, 0).max()))
+        print('cr = %d' %(np.sum(cr, 0).max()))
 
-
+def main2508Analysis():
+    sp = SpiderTest()
+    # tt_list = ['20210812_123640-20210812_Q_2x2_05',
+    #            '20210812_123640-20210812_Q_2x2_10',
+    #            '20210812_123640-20210812_G_2x2_05',
+    #            '20210812_123640-20210812_G_2x2_10',
+    #            '20210812_123640-20210812_G_2x2_15',
+    #            '20210812_123640-20210812_G_2x2_20',
+    #            '20210812_123640-20210812_Q_3x3_05',
+    #            '20210812_123640-20210812_Q_3x3_10']
+    tt_list = ['20210812_124218-20210812_Q_2x2_05',
+               '20210812_124218-20210812_Q_2x2_10',
+               '20210812_124218-20210812_G_2x2_05',
+               '20210812_124218-20210812_G_2x2_10',
+               '20210812_124218-20210812_G_2x2_15',
+               '20210812_124218-20210812_G_2x2_20',
+               '20210812_124218-20210812_Q_3x3_05',
+               '20210812_124218-20210812_Q_3x3_10']
+    for tt in tt_list:
+        print('Analysing tt = %s' %tt)
+        cp, cr, cp_val, num_std = sp.analysis(tt)
+        print('cp = %d' %(np.sum(cp, 0).max()))
+        print('cr = %d' %(np.sum(cr, 0).max()))
+        print('num_std = %d' %num_std)
