af35eceb75115cf424fc59c93a247a45bd1332e5
cselmi <rm@chiara-mac.fritz.box>
2022-02-09 12:32:00 +0100

    test req check e sistemazine iffmaker

---
+++ test/analyzers/test_requirement_analyzer.py
@@ -0,0 +1,32 @@
+'''
+Authors
+  - C. Selmi:  written in 2022
+'''
+import unittest
+import os
+from m4.analyzers import requirement_analyzer as req_check
+from test.test_helper import testDataRootDir
+from m4.ground import read_data
+
+class TestRequirementAnalyzer(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testReadImageAndAnalysis(self):
+        image_location = os.path.join(testDataRootDir(),
+                                      'ima_for_req.fits')
+        image = read_data.read_phasemap(image_location, ext=0)
+
+        pscale = None
+        step = 10000
+        n_patches = None
+        slope = req_check.test242(image, pscale)
+        diff_piston = req_check.diffPiston(image)
+        roc = req_check.test283(image, pscale, step)
+        rem_31 = req_check.test243(image, 0.015, pscale, step, n_patches)
+        rms_500 = req_check.test243(image, 0.1, pscale, step, n_patches)
+        
