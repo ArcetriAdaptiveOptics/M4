70773ef97d8fda5ab0566871b30233c0061f3078
cselmi <rm@192.168.1.4>
2022-02-10 12:32:47 +0100

    test start version 2

--- test/configuration/test_start.py
+++ test/configuration/test_start.py
@@ -4,6 +4,7 @@ Authors
 '''
 import os
 import unittest
+import mock
 from m4.configuration.start import create_ott
 from m4.ott_sim.fake_parabola_slider import FakeParabolaSlider
 from m4.ott_sim.fake_interferometer import FakeInterferometer
@@ -15,6 +16,10 @@ class TestStart(unittest.TestCase):
 
     def testCreationWithSimulatedDevices(self):
         #aFakeConfig.simulated = 1
-        ott, interf = create_ott(os.path.join(testDataRootDir(), 'base', 'ottSim', 'testConf.yaml'))
+        ott, interf = create_ott(os.path.join(testDataRootDir(), 'base', 'Configurations', 'testConf.yaml'))
         self.assertIsInstance(ott.parabolaSlider, FakeParabolaSlider)
         self.assertIsInstance(interf, FakeInterferometer)
+
+    #@mock.patch('m4.devices.interferometer.I4d4020', autospec=True)
+    def testCreationWhitFakePathInYaml(self):
+        ott, interf = create_ott(os.path.join(testDataRootDir(), 'base', 'Configurations', 'testConf2.yaml'))
