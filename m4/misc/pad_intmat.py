c19a0806fb2ba2fd5a6b30903c6cf45238d9f4f4
cselmi <rm@192.168.1.4>
2022-09-27 11:45:37 +0200

    sistemazione formale di pad_intmat e comment matrice che non esite

--- m4/misc/pad_intmat.py
+++ m4/misc/pad_intmat.py
@@ -1,7 +1,12 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
 from m4.ground import read_data
 from m4.ground import zernike as zern
 from m4.ground import geo as geo
 from astropy.io import fits
+
+
 ic=read_data.InterferometerConverter()
 base = '/mnt/data/M4/Data/M4Data/OPTData/PAR_Meas/20220802/'
 
@@ -15,11 +20,14 @@ def parzmodes(fname):
     img2= np.ma.masked_array(img.data, -1*mask1+1)
     zlist = [1,2,3,4,5,6,7,8,9,10,11]
     ccx, zmatx = zern.zernikeFit(img2, zlist)
-    clf(); imshow(img2); colorbar(); title(fname)
+    plt.clf()
+    plt.imshow(img2)
+    plt.colorbar()
+    plt.title(fname)
     foc = ccx[3]
     ast = ccx[4:6]
     tref=ccx[8:10]
-    cc = array((foc, ast, tref))
+    cc = np.array((foc, ast, tref))
     return cc
 
 def rotmat(beta):
@@ -54,16 +62,16 @@ for ii in range(5):
     pz00[:,ii] = parzmodes(p00[ii]).flatten()
     pz10[:,ii] = parzmodes(p10[ii]).flatten()
     pz50[:,ii] = parzmodes(p50[ii]).flatten()
-    pz0bis[:,ii] = parzmodes(p0bis[ii]).flatten()    
+    pz0bis[:,ii] = parzmodes(p0bis[ii]).flatten()
     pz60[:,ii] = parzmodes(p60[ii]).flatten()
     pz70[:,ii] = parzmodes(p70[ii]).flatten()
 
-pz0m = mean(pz00, 1)
-pz1m = mean(pz10, 1)
-pz5m = mean(pz50, 1)
-pz0bism = mean(pz0bis,1)
-pz6m = mean(pz60, 1)
-pz7m = mean(pz70, 1)
+pz0m = np.mean(pz00, 1)
+pz1m = np.mean(pz10, 1)
+pz5m = np.mean(pz50, 1)
+pz0bism = np.mean(pz0bis,1)
+pz6m = np.mean(pz60, 1)
+pz7m = np.mean(pz70, 1)
 
 z1m = pz1m - pz0m
 z5m = pz5m - pz0m
@@ -79,17 +87,17 @@ cmat = cmat[:,1::] #delete this line for focus
 cmat = cmat.T
 
 invmat = np.linalg.pinv(cmat)
-cmd0 = matmul(invmat, pz0m)
-pz0m - matmul(cmat, cmd0)
-cmd1 = array((cmd0, cmd0))
+cmd0 = np.matmul(invmat, pz0m)
+pz0m - np.matmul(cmat, cmd0)
+cmd1 = np.array((cmd0, cmd0))
 
 rmat = rotmat(120)
-cmat1 = matmul(cmat, rmat.T)
+cmat1 = np.matmul(cmat, rmat.T)
 
-ccm = concatenate((cmat, mat1), 1)
-
-invmat2 = np.linalg.pinv(ccm)
-cmd2 = matmul(invmat2, pz0m)
+# ccm = np.concatenate((cmat, mat1), 1)
+# 
+# invmat2 = np.linalg.pinv(ccm)
+# cmd2 = np.matmul(invmat2, pz0m)
 
 
 
