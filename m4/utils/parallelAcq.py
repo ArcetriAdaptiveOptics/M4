d68e789e46586943d4f3a6147ab613d881fc42ed
cselmi <rm@192.168.1.4>
2022-10-04 11:59:28 +0200

    modifiche su funzione semi-sconosciuta.

--- m4/utils/parallelAcq.py
+++ m4/utils/parallelAcq.py
@@ -1,37 +1,28 @@
-from m4.configuration import start
+''' Da dove viene? A cosa serve?
+RIVEDERE
+'''
 import time
-import os
-import numpy as np
-from astropy.io import fits as pyfits
-from matplotlib import pyplot as plt
-from m4.configuration.ott_parameters import OpcUaParameters
-from m4.ground.opc_ua_controller import OpcUaController
-from m4.configuration.config import fold_name
-from m4.alignment import Alignment
-from m4.ground import tracking_number_folder
-from m4.ground import zernike
-from m4.ground.timestamp import Timestamp
-#from m4.utils import accelerometer_data as acc
 import threading
 import logging
 
 
-from m4.configuration import start as startOTT
 
+class ParallelAcq():
 
-class parallelAcq():
+    def __init__(self, ott, interf, time2acq=3):
 
-    def __init__(self,time2acq=3):
-
-        logging.basicConfig(level=logging.DEBUG,format='[%(levelname)s] (%(threadName)-10s) %(message)s')
+        logging.basicConfig(level=logging. DEBUG,
+                            format='[%(levelname)s] (%(threadName)-10s) %(message)s')
         self._threads = []
         self._time2acq = time2acq
+        self._nframespersec = '?'
         #self._acc = acc.Accelerometers() //to update with new class
-        self._ott, self._interf =  startOTT.create_ott()
+        self._ott = ott
+        self._interf = interf
         self._accTN = ""
         self._interfTN = ""
         self._zabbixTN = ""
-        
+
         self.resetThreads()
 
     def resetThreads(self):
@@ -55,18 +46,16 @@ class parallelAcq():
         logging.debug("start acc acquisition")
         self._accTN = self._ott.accelerometer.acquireData(self._time2acq)
         logging.debug("stop acc acquisition")
-    
+
 
     def worker_i4d(self):
         logging.debug("start i4d")
         nframes = self._time2acq/self._nframespersec
         self._interfTN=self._interf.acquire_phasemap(nframes)
         logging.debug("stop i4d")
-    
+
     def worker_zbx(self):
         logging.debug("start zbx")
         #self._ott.temperatures.getData()  (time2acq)
         time.sleep(self._time2acq+3)
         logging.debug("stop zbx")
-
-
