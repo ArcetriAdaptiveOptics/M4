04834095b6d9cfa1adff37d9ce84ad6e1d5e42c6
cselmi <rm@192.168.1.6>
2022-11-02 14:20:00 +0100

    test per parabola identification

--- test/utils/test_parabolaIdentification.py
+++ test/utils/test_parabolaIdentification.py
@@ -4,7 +4,7 @@ Authors
 '''
 import unittest
 import os
-from m4.utils.parabola_identification import ParabolaCirculaPupilFromZernike
+from m4.utils.parabola_identification import ParabolaCirculaPupil
 
 TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'img_0000.fits')
 
@@ -12,6 +12,6 @@ class Test(unittest.TestCase):
 
     @unittest.skip('Non funziona il linalg.eig (non va da github)')
     def testFiduciali(self):
-        pz = ParabolaCirculaPupilFromZernike()
+        pz = ParabolaCirculaPupil()
         image = pz._imaTest(TESTDATA_FILENAME)
-        coef1, coef2 = pz.zernike_on_parabola(image)
+        circle_mask = pz.par_mask_on_ott(image)
