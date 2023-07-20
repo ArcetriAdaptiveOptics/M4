0951eae053fa975ea1f7860bf035a9e8c944bd42
ChiaraSelmi <chiara.selmi@inaf.it>
2023-07-20 13:02:50 +0200

    Plot dei risultati

--- m4/gui/accelerometers_GUI.py
+++ m4/gui/accelerometers_GUI.py
@@ -5,8 +5,9 @@ Authors
 import numpy as np
 from m4.ground.package_data import data_root_dir
 from m4.analyzers.accelerometers_data_analyzer import AccelerometersDataAnalyzer
-from guietta import Gui, G, _, ___, HB
+from guietta import Gui, G, _, ___, HB, MA
 import sys
+from m4.configuration.ott_parameters import OpcUaParameters
 
 class Runner():
     '''
@@ -31,12 +32,31 @@ class Runner():
 
     def _setUp(self):
 
-        def setPlot(gui):
+        def setPlot(gui, an):
             '''
             image: numpy array
                 image of the tower
             '''
-            gui.heart_empty_30.beat()
+            spe1 = an._spe[:, 1:]
+            freq1 = an._freq[1:]
+            
+            ax = gui.plot.ax
+            ax.clear()
+            ax.set_title(an.tt)
+            label_list = []
+            for i in OpcUaParameters.accelerometers_plc_id:
+                ss = 'Ch-' + str(i) + ' ' + OpcUaParameters.accelerometrs_directions[i - 1]
+                label_list.append(ss)
+            for i in range(spe1.shape[0]):
+                prova = ax.plot(freq1, np.abs(spe1[i, :]), '-')
+                ax.legend(prova, label_list[i], loc=0)
+                #gui.proxy('label%d'%i).set('%s' % label_list[i])
+                ax.figure.canvas.draw()
+            ax.set_xlim([0, 100])
+            #gui.plot.set_clim(vmin=0, vmax=100)
+            ax.set_xlabel('Freq[Hz]')
+            ax.set_ylabel('Amplitude Spectrum |m/s2|')
+            #gui.heart_empty_30.beat()
 
         def Start_Acquisition(gui, *args):
             ''' Function to reload tower data and image
@@ -48,7 +68,9 @@ class Runner():
         def Start_Analysis(gui, *args):
             track_num = np.str(gui.track_num)
             an = AccelerometersDataAnalyzer(track_num)
-            an.readAndShow()
+            an._spe, an._freq = an._acc.power_spectrum()
+            an.datah5 = an._acc.datah5
+            setPlot(gui, an)
 
         gui_acquisition = Gui(['Set recording time', '__rec_sec__', _, '[s]'],
                               [Start_Acquisition, _, _, _],
@@ -56,7 +78,8 @@ class Runner():
                               [ _, _, _, HB('heart_empty_30.png', 'heart_full_30.png')], images_dir=data_root_dir())
         
         gui_analysis = Gui(['Set tracking number', '__track_num__'],
-                              [Start_Analysis, _])
+                              [Start_Analysis, _],
+                              [MA('plot'), _])
 
         self.gui = Gui([ G('Acc_acquisitions'), G('Acc_analysis')])
         self.gui.Acc_acquisitions = gui_acquisition
