9ae574243f990e8a7bb2aad47ad89312b0ac0566
marco.xompero <marco.xompero@inaf.it>
2024-06-14 17:39:41 +0200

    Merge branch 'master' of https://github.com/ArcetriAdaptiveOptics/M4

--- test/utils/test_opticalAlignment.py
+++ test/utils/test_opticalAlignment.py
@@ -7,80 +7,130 @@ import unittest
 import os
 import numpy as np
 from m4.utils.influence_functions_maker import IFFunctionsMaker
-from test.helper_test_library import testDataRootDir
 from m4.configuration.start import create_ott
 from m4.ott_sim.fake_interferometer import FakeInterferometer
 from m4.utils.optical_calibration import OpticalCalibration
 from m4.configuration.create_ott import OTT as ottobj
+from m4.ott_sim.fake_deformable_mirror import FakeM4DM
+from m4.utils.optical_alignment import OpticalAlignment
 import mock
 
 
 class TestOpticalAlignment(unittest.TestCase):
 
+    class FakeOpen:
+        def __init__(self, filename, mode):
+            # if filename == "config.json" and mode == "r":
+            #    raise FileNotFoundError()
+            self.filename = filename
+            self.mode = mode
+            self.write_calls = 0
 
-    def optical_calibration_init_mock(self, tt, dm, interf, ott):
-        """The mock  constructor """
-        self._logger = logging.getLogger('OPT_CALIB:')
-        self._interf = interf
-        self._ott = ott
-        #start
-        self._nPushPull = None
-        self._commandAmpVector = None
-        self._who = None
-        #from calibration
-        self._dofIndex = None
-        self._commandMatrix = None
-        self._commandList = None
-        self.tt = None
-        #from analysis
-        self._cube = None
-        self._mask = None
-        self._intMat = None
+        def read(self):
+            return b"{}"
 
-        self._fullCommandMatrix=None
-        self._fullCube=None
+        def write(self, write_string):
+            self.write_calls += 1
+            # assert self.write_calls <= 1
+            # assert write_string == "{}"
 
-    def setUp(self):
-        self._fake_header = {"WHO": "M4", "NPUSHPUL": 1}
-        self._fake_data = [np.ones((10, 10)) for i in range(4)]
+        def close(self):
+            if self.mode == "w":
+                assert self.write_calls == 1
 
-    @mock.patch.object(m4.utils.optical_calibration.OpticalCalibration, "__init__", optical_calibration_init_mock)
-    @mock.patch("m4.utils.optical_calibration.OpticalCalibration",autospec=True)
-#    @mock.patch("m4.configuration.start", autospec=True)
+        def __enter__(self):
+            return self
+
+        def __exit__(self, *args, **kwargs):
+            pass
+
+    def testDataRootDir(self):
+        return os.path.join(os.path.dirname(__file__), "../data")
+
+    def tearDown(self):
+        self._cal
+        self._ott
+        self._interf
+        self._dm
+
+    #        par_position = self._ott.parabola.getPosition()
+
+    #        rm_position = self._ott.referenceMirror.getPosition()
+    #        tout = self.selectModesInIntMatAndRecConstruction(
+    #        self._intMat, self._rec, self._cmat = tout
+    #        makedirs
+    #        self._interf.save_phasemap(dove, name, img)
+
+    @mock.patch("m4.utils.optical_calibration.OpticalCalibration", autospec=True)
     @mock.patch("astropy.io.fits.getheader", autospec=True)
     @mock.patch("astropy.io.fits.open", autospec=True)
-    def testCreate(self, mock_cal, mock_header, mock_open):
+    @mock.patch("m4.configuration.start.FakeM4DM", autospec=True)
+    def setUp(self, mock_interf, mock_open, mock_header, mock_cal):
+        self._fake_header = {"WHO": "M4", "NPUSHPUL": 1}
+        self._fake_data = [np.ones((10, 10)) for i in range(4)]
         tt_cal = "20220309_142454.fits"
         mock_header.return_value = self._fake_header
         mock_open.return_value = self._fake_data
-        self.cal = OpticalCalibration.loadCalibrationObjectFromFits(tt_cal)
-        self.interf, self.ott, self.dm = create_ott(
-            os.path.join(testDataRootDir(), "base", "Configurations", "testConf.yaml")
+        mock_cal.return_value = OpticalCalibration(mock_interf, None)
+        self._cal = OpticalCalibration.loadCalibrationObjectFromFits(tt_cal)
+        self.assertIsInstance(self._cal, OpticalCalibration)
+
+        self._ott, self._interf, self._dm = create_ott(
+            os.path.join(
+                self.testDataRootDir(), "base", "Configurations", "testConf.yaml"
+            )
         )
 
-    #    self.assertIsInstance(self.interf, FakeInterferometer)
-    #    self.assertIsInstance(self.ott, ottobj)
-    #    self.dom(self.dm, dm)
-    #    pass
+        self.assertIsInstance(self._ott, ottobj)
+        self.assertIsInstance(self._interf, FakeInterferometer)
+        self.assertIsInstance(self._dm, FakeM4DM)
 
-
-#    @mock.patch(
-#        "m4.utils.influence_functions_maker.IFFunctionsMaker._storageFolder",
-#        autospec=True,
-#    )
-#    @mock.patch("astropy.io.fits.writeto", autospec=None)
-#    @mock.patch("m4.type.modalAmplitude.ModalAmplitude._storageFolder", autospec=True)
-#    @mock.patch("m4.type.modalBase.ModalBase._storageFolder", autospec=True)
-#    @mock.patch("m4.ground.tracking_number_folder.os.makedirs", autospec=True)
-#    @mock.patch("m4.type.commandHistory.CmdHistory.saveInfo", autospec=True)
-#    def testReload(self, mock_folder):
-#        tt = "20220309_142454"
-#        mock_folder.return_value = os.path.join(
-#            testDataRootDir(), "base", "M4Data/OPTData/IFFunctions"
-#        )
-#        IFFunctionsMaker.loadInfo(tt, 0)
-
-#write main class to run setUp and testCreate
-if __name__ == "__main__":
-    unittest.main()
-
+    @mock.patch("m4.utils.optical_calibration.OpticalCalibration", autospec=True)
+    @mock.patch("astropy.io.fits.getheader", autospec=True)
+    @mock.patch("astropy.io.fits.open", autospec=True)
+    @mock.patch.object(
+        OpticalAlignment,
+        "selectModesInIntMatAndRecConstruction",
+        return_value=(np.identity(10), np.identity(10), np.identity(10)),
+    )
+    @mock.patch("os.makedirs", autospec=True)
+    @mock.patch("m4.utils.optical_alignment.open", FakeOpen, create=True)
+    @mock.patch("m4.configuration.start.FakeM4DM", autospec=True)
+    @mock.patch("m4.configuration.start.create_ott", autospec=True)
+    @mock.patch.object(
+        FakeInterferometer,
+        "acquire_phasemap",
+        return_value=np.ma.masked_array(np.ones((10, 10))),
+    )
+    @mock.patch.object(
+        FakeInterferometer,
+        "save_phasemap",
+        return_value=np.ma.masked_array(np.ones((10, 10))),
+    )
+    def testOptAlign(
+        self,
+        mock_sphm,
+        mock_aphm,
+        mock_cott,
+        mock_interf,
+        mock_makedirs,
+        # mock_open,
+        mock_selectModes,
+        mock_fits_open,
+        mock_header,
+        mock_cal,
+    ):
+        self._ott, self._interf, self._dm = create_ott(
+            os.path.join(
+                self.testDataRootDir(), "base", "Configurations", "testConf.yaml"
+            )
+        )
+        tt_cal = "20220309_142454.fits"
+        self._align = OpticalAlignment(tt_cal, self._ott, FakeInterferometer())
+        self._align.opt_aligner(
+            n_images=10,
+            delay=1,
+            zernike_to_be_corrected=[1, 2, 3],
+            dof_command_id=None,
+            subapOffsets=False,
+        )
