1c95d148fbc22292ee879c5a1e5a71814f014382
runa.briguglio <runa.briguglio@inaf.it>
2024-09-03 14:02:44 +0200

    description about flattening functionalities

--- scripts/misc/Flattening/runa_testflattening.py
+++ scripts/misc/Flattening/runa_testflattening.py
@@ -1,3 +1,4 @@
+#in generale, guarda /home/runa/git/M4/m4/flattening.py per confronto
 #non c'è import
 tnIntmat = 'xxx'
 ff = Flattening(tnIntmat) #servono solo i dati. i dati relativi al DM sono importato con i dati della misura, anche perchè la cmdmatrix deve essere quella salvata, non quella attuale.
@@ -5,3 +6,19 @@ nmodes = 100
 fmodes = arange(0,nmodes)
 nframes = 20
 tnflat = ff.flatten(fmodes, nframes)
+
+
+#come importare il Rec
+from m4.analyzers import compute_reconstructor as cmprec
+rec = ComputeReconstructor(tn)
+recmat =rec.run(nmodes)
+#chiedere a MX di modificare l'init di cmprec in modo che serva solo il TN
+#oppure che capisca se gli hai passato il tn o i dati (utile se ripeti diverse volte??)
+
+#chiedere a MX di implementare la modalità "seleziona quanti modi tagliare" e fa vedere lo spettro
+
+#chiedere a MX di implementare il filtraggio di N Zernike su analysis mask e predisporre per ROI e operazioni per rimozione tilt globale e pistone
+
+#il filtraggio degli zernike può avvenire all'init? forse no, meglio farlo come operazione separata e prevedere due variabili "interne": _intMat e _intMatFiltered, e la Rec si fa sempre sulla filtered
+
+
