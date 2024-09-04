5b3c7ad12abe58bb97224c4943204b8d2ddf59f1
pietroferraiuolo <pietro.ferraiuolo@inaf.it>
2024-09-04 16:17:08 +0200

    writing the flattening procedure

--- scripts/misc/Flattening/flattening.py
+++ scripts/misc/Flattening/flattening.py
@@ -12,7 +12,7 @@ from m4.devices import deformable_mirror as dm
 from m4.dmutils import iff_processing as ifp
 from m4.ground import read_data as rd
 from m4.utils import osutils as osu
-from analyzers import compute_reconstructor as crec
+from m4.analyzers import compute_reconstructor as crec
 #  ---- leggere flattening.py come possibile riferimento -----
 
 class Flattening:
@@ -26,31 +26,36 @@ class Flattening:
     flat
     ...
     """
-
     def __init__(self, tn):
         """The Constructor"""
-        self._tn = tn
-        self._path = os.path.join(osu.OPTDATA ,self._tn)
-        self._intMat = self._loadIntMat()
-        #self.dm = dm.AdOpticaDM()?? no, non serve importare il DM
-        self._cmdMat = self._loadCmdMat()
-        self._recMat  = None
-        self._frameCenter = None
-        self._shape2flat = None
-        self._flatOffset = None
-        self._cavityOffset = None
-        self._synthFlat = None
-        self._flatResidue = None
-        self._flatResult = None
+        self._tn            = tn
+        self._path          = os.path.join(ifp.intMatFold, self._tn)
+        self._intMat        = self._loadIntMat()
+        self._cmdMat        = self._loadCmdMat()
+        self._rec           = crec.ComputeReconstructor(self._intMat)
+        self._recMat        = self._rec.run()
+        self._frameCenter   = None
+        self._shape2flat    = None # Immagine acquisita o fornita, come? 
+        self._flatOffset    = None
+        self._cavityOffset  = None
+        self._synthFlat     = None
+        self._flatResidue   = None
+        self._flatResult    = None
         self._flatteningModes = None
+
+    def flatten(self, nmodes):
         pass
 
-    def flatten(nmodes):
-        pass
+    def computeFlatCmd(self):
+        # Logica
+        cmd_amp = -np.dot(self._recMat.flatten(), self._shape2flat.compressed())
+        flat_cmd = np.dot(self._cmdMat, cmd_amp)
+        self._flatResult = flat_cmd
+        return flat_cmd
 
     def _loadIntMat(self):
         cube = os.path.join(self._path, ifp.cubeFile)
-        with open(os.path.join(cube, ifp.flagFile), 'r', encoding='utf-8') as f:
+        with open(os.path.join(self._path, ifp.flagFile), 'r', encoding='utf-8') as f:
             flag = f.read()
         if 'filtered' in flag:
             intmat = rd.read_phasemap(cube)
@@ -59,18 +64,19 @@ class Flattening:
         return intmat
 
     def _loadCmdMat(self):
-        cmdMat = rd.readFits_data(osu.path.join(self._path, ifp.cmdMatFile))
+        cmdMat = rd.readFits_data(os.path.join(self._path, ifp.cmdMatFile))
         return cmdMat
 
     def _loadFrameCenter(self):
         frame_center = rd.readFits_data('data')
         return frame_center
 
-    def _computeRec(self, nmodes, zernike2remove):
-        pass
-
     def _registerShape(self, shape):
         xxx=None
         dp = ifp.findFrameOffset(self._tn,xxx)
         #cannot work. we should create a dedicated function, not necessarily linked to IFF or flattening
         pass
+
+    def load_image2shape(self, img):
+        self._shape2flat = img
+        return
