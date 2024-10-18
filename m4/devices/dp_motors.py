1f29bcd3515d01f3df5efe69802c34d3e2bb6df4
pietroferraiuolo <pietro.ferraiuolo@inaf.it>
2024-10-18 13:56:51 +0200

    Developing dp_motors

--- m4/devices/dp_motors.py
+++ m4/devices/dp_motors.py
@@ -1,24 +1,24 @@
-'''
-Authors
-  - P. Ferraiuolo, R. Briguglio: written in 2024
-'''
-#import logging
+"""
+Author(s)
+---------
+    - Pietro Ferraiuolo: written in 2024
+    - Runa Briguglio: written in 2024
+"""
+import zmq
 import numpy as np
+from m4.ground import logger_set_up as logger
 from m4.configuration.ott_parameters import OttParameters
 from m4.devices.base_m4_exapode import BaseM4Exapode
 
-'''
-The kinematics is written in the form (normalization-less):
+# The kinematics is written in the form (normalization-less):
 
-          C     ^Y
-                |
-       A     B  -->X
-  Pist  Tx  Ty
-A    1  -1  -1
-B    1   1  -1
-C    1   0   1
-'''
-
+#           C     ^Y
+#                 |
+#        A     B  -->X
+#   Pist  Tx  Ty
+# A    1  -1  -1
+# B    1   1  -1
+# C    1   0   1
 
 class DpMotors(BaseM4Exapode):
     ''' Class for M4 control via opc ua??
@@ -33,15 +33,19 @@ class DpMotors(BaseM4Exapode):
     def __init__(self, DPinterface):
         """The constructor """
         self._dpinterface = DPinterface
-        self._logger = logging.getLogger('DPMotors')
         self._kinematrix = np.array([[1,1,1],[-1,1,0],[-1,-1,1]])
         self._invkinematrix = np.linalg.inv(self._kinematrix)
+        self._context = None
+        self._socket = None
+        self.remote_ip = 0
+        self.remote_port = 0
+        logger.set_up_logger('path/dpMotors.log', 10)
 
     def getPosition(self):
         ''' to be implemented
         '''
-        pp = _getMotorPosition()
-        kk = _motor2kinematics(pp)
+        pp = self._getMotorPosition()
+        kk = self._motor2kinematics(pp)
         pos = np.zeros(6)
         pos[OttParameters.M4_DOF] = kk[1:]
         print(pos)
@@ -54,7 +58,7 @@ class DpMotors(BaseM4Exapode):
         v = np.array([0,absolute_position_in_mm[OttParameters.M4_DOF]])
         vm = np.dot(v,self._kinematrix)
         print(vm)
-        _setMotorPosition(vm)
+        self._setMotorPosition(vm)
         pass
 
 
@@ -85,4 +89,23 @@ class DpMotors(BaseM4Exapode):
         #return abc
         pass
     
-    
+    def _connectBusBox(self):
+        try:
+            self._context = zmq.Context()
+            self._socket = self._context.socket(zmq.PAIR)
+            self._socketconnect(f"tcp://{self.remote_ip}:{self.remote_port}")
+            return True
+        except Exception as e:
+            logger.log(f"ConnectZMQ: {e}")
+            return False
+
+    def _disconnectBusBox(self):
+        try:
+            self._socket.disconnect(f"tcp://{self.remote_ip}:{self.remote_port}")
+            self._socket.close()
+            self._context.term()
+            return True
+        except Exception as e:
+            logger.log(f"DisconnectZMQ: {e}")
+            return False
+        pass
