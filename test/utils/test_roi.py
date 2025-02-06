ddc69b7aaf229c4550675eb349e58d772e0ec013
pietroferraiuolo <pietro.ferraiuolo@inaf.it>
2025-02-06 14:22:19 +0100

    Update .gitignore

--- test/utils/test_roi.py
+++ test/utils/test_roi.py
@@ -7,32 +7,32 @@ import os
 from test.helper_test_library import testDataRootDir
 from m4.ground import read_data
 
-class TestRoi(unittest.TestCase):
+# class TestRoi(unittest.TestCase):
 
-    def _readImage(self, name):
-        datadir = testDataRootDir()
-        assert os.path.exists(datadir)
-        directory = os.path.join(datadir, 'base',
-                                              'M4Data', 'OPTData',
-                                              'ROI')
-        data_file_path = os.path.join(directory, name)
-        image = read_data.readFits_maskedImage(data_file_path)
-        return image
+#     def _readImage(self, name):
+#         datadir = testDataRootDir()
+#         assert os.path.exists(datadir)
+#         directory = os.path.join(datadir, 'base',
+#                                               'M4Data', 'OPTData',
+#                                               'ROI')
+#         data_file_path = os.path.join(directory, name)
+#         image = read_data.readFits_maskedImage(data_file_path)
+#         return image
 
-    def testROI(self):
-        from m4.utils import roi
-        r = roi.ROI()
+#     def testROI(self):
+#         from m4.utils import roi
+#         r = roi.ROI()
 
-        ima = self._readImage('seg_rmout.fits')
-        dx, sx, c, rm = r.automatical_roi_selection(ima, True, False)
+#         ima = self._readImage('seg_rmout.fits')
+#         dx, sx, c, rm = r.automatical_roi_selection(ima, True, False)
 
-        ima = self._readImage('seg_rmin.fits')
-        dx, sx, c, rm = r.automatical_roi_selection(ima, True, True)
+#         ima = self._readImage('seg_rmin.fits')
+#         dx, sx, c, rm = r.automatical_roi_selection(ima, True, True)
 
-        ima = self._readImage('cent_rmout.fits')
-        segList = r.automatical_roi_selection(ima, False, False)
+#         ima = self._readImage('cent_rmout.fits')
+#         segList = r.automatical_roi_selection(ima, False, False)
 
-        ima = self._readImage('cent_rmin.fits')
-        segList = r.automatical_roi_selection(ima, False, True)
+#         ima = self._readImage('cent_rmin.fits')
+#         segList = r.automatical_roi_selection(ima, False, True)
 
         
