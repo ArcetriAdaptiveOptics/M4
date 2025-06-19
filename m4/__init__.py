c0f99f13a7f7918c9c5bcc03c8e4b73130ff9287
Runa Briguglio <runa.briguglio@inaf.it>
2025-06-19 10:15:02 +0200

    push da montagna

--- m4/__init__.py
+++ m4/__init__.py
@@ -1,5 +1,4 @@
-### restore the following lines to implement M4 init
-'''
+
 from opticalib import PhaseCam, AdOpticaDM
 from m4.configuration import ott
 
@@ -7,4 +6,3 @@ interf = PhaseCam('6110')
 dm = AdOpticaDM()
 
 ott = ott.create_ott()
-'''
