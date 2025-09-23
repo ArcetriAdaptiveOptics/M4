7ea5db6e8df99c08b6c17afd80b8308612359e3d
Runa Briguglio <runa.briguglio@inaf.it>
2025-09-23 13:00:51 +0200

    ìmerged

--- m4/alignment.py
+++ m4/alignment.py
@@ -86,8 +86,6 @@ class OttAligner(_al.Alignment):
         if applycmd:
             ntn = _newtn()
             print("Applying correction command...")
-            # self._apply_command(f_cmd)
-            # Tentativo 1
             super()._apply_command(f_cmd)
             coeffs_f = self._zern_routine(self._acquire[0](nframes=n_frames))
             self._write_correction_log(modes2correct, zern2correct, ntn, coeffs_i, coeffs_f)
